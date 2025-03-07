64c690e19ba44e6cf8c1b599950dec7b32b0176e
jeevitha.r@mastechdigital.com <jeevitha.r@mastechdigital.com>
2025-03-07 13:09:52 +0000

    updated

--- models/stg_person_details.sql
+++ models/stg_person_details.sql
@@ -1,17 +1,18 @@
+CREATE OR REPLACE TABLE `bigquery-poc-443509`.`staging`.`stg_person_details`
+AS 
 WITH stg_person_details AS (
-SELECT 
-    id,  
-    first_name,
-    last_name,
-    LOWER(email) AS email,
-    CASE 
-        WHEN gender = 'Male' THEN 'M' 
-        WHEN gender = 'Female' THEN 'F' 
-        ELSE NULL 
-    END AS gender,
-    joining_date,
-    CONCAT(first_name, ' ', last_name) AS full_name
-    FROM stg_person_details
+    SELECT 
+        id,  
+        first_name,
+        last_name,
+        LOWER(email) AS email,
+        CASE 
+            WHEN gender = 'Male' THEN 'M' 
+            WHEN gender = 'Female' THEN 'F' 
+            ELSE NULL 
+        END AS gender,
+        joining_date,
+        CONCAT(first_name, ' ', last_name) AS full_name
+    FROM `bigquery-poc-443509`.`curated`.`stg_person_details`
 )
-
 SELECT * FROM stg_person_details;
