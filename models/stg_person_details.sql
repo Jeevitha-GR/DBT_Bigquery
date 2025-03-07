fa623932ee12dd35c11baa442d54eaf6ffa1d3bb
jeevitha.r@mastechdigital.com <jeevitha.r@mastechdigital.com>
2025-03-07 13:35:58 +0000

    updated

--- models/stg_person_details.sql
+++ models/stg_person_details.sql
@@ -1,16 +1,13 @@
-WITH stg_person_details AS (
-    SELECT 
-        id,  
-        first_name,
-        last_name,
-        LOWER(email) AS email,
-        CASE 
-            WHEN gender = 'Male' THEN 'M' 
-            WHEN gender = 'Female' THEN 'F' 
-            ELSE NULL 
-        END AS gender,
-        joining_date,
-        CONCAT(first_name, ' ', last_name) AS full_name
-    FROM bigquery-poc-443509.curated.stg_person_details
-)
-SELECT * FROM stg_person_details;
+SELECT 
+    id,  
+    first_name,
+    last_name,
+    LOWER(email) AS email,
+    CASE 
+        WHEN gender = 'Male' THEN 'M' 
+        WHEN gender = 'Female' THEN 'F' 
+        ELSE NULL 
+    END AS gender,
+    joining_date,
+    CONCAT(first_name, ' ', last_name) AS full_name
+    FROM bigquery-poc-443509.curated.stg_person_details;
